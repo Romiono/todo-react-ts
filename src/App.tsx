245bdd99a56229db6f7a067c04be637ca740c55e
Romiono <roma.serebrennikov.00@mail.ru>
2024-03-07 00:40:56 +0300

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Main from "./components/Aplication/Main.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+// import React from "react";
 
 function App() {
-
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <Main/>,
+        },
+    ]);
     return (
         <div className={'app'}>
-            <Main/>
+            <RouterProvider router={router}/>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,27 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import {Provider} from "react-redux";
 import {setupStore} from "./store/store.ts";
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
-import Main from "./components/Aplication/Main.tsx";
 
 const store = setupStore();
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main/>,
-    },
-    {
-        path: '/main',
-        element: <App/>
-    },
-
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <React.StrictMode>
-            <RouterProvider router={router}/>
+            <App/>
         </React.StrictMode>
     </Provider>
 )
