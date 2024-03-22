3137e40adf1ea0d391bbfefc76d11c3f4f287d16
Romiono <roma.serebrennikov.00@mail.ru>
2024-03-22 08:53:45 +0300

    todo page update

--- src/components/Todo/TodoPage.tsx
+++ src/components/Todo/TodoPage.tsx
@@ -5,16 +5,21 @@ const TodoPage = () => {
     const {id} = useAppSelector(state => state.curentTodoReducer);
     const {data} = todoAPI.useFetchTodoIdQuery(id);
 
-    if(data) {
+    if (data) {
         return (
-            <div>
-                <h3>Название: {data.title}</h3>
-                <h4>Описание: {data.description}</h4>
-                {data.checked ?
-                    <h5>Статус: Выполнено</h5>
-                    :
-                    <h5>Статут: Ожидает выполнения</h5>
-                }
+            <div className={'p-5'}>
+                <div className={'mb-5 flex flex-col gap-0.5'}>
+                    <h3 className={'text-3xl font-black'}>Название: {data.title}</h3>
+                    <h4 className={'text-xl font-bold'}>Описание: {data.description ?
+                        data.description :
+                        <button className={'text-sm'}>добавить описание</button>}
+                    </h4>
+                    <h5 className={'font-bold'}>Статус: {data.checked ?
+                        <span className={'text-green-500'}>Выполнено</span> :
+                        <span className={'text-red-500'}>Ожидает выполнения</span>}
+                    </h5>
+                </div>
+                <button>Добавить картинку</button>
             </div>
         );
     }
