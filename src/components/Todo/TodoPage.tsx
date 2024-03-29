a953e3e3ff348f5c0f39a0c7ba571f1031da2275
Romiono <roma.serebrennikov.00@mail.ru>
2024-03-29 23:48:53 +0300

    todoPage update

--- src/components/Todo/TodoPage.tsx
+++ src/components/Todo/TodoPage.tsx
@@ -14,7 +14,7 @@ const TodoPage = () => {
     const addDescription = () => {
         try {
             if (data) {
-                patch({id: data.id, title:data.title, description, checked: data.checked, favorite: data.favorite});
+                patch({...data, description});
             }
         } catch {
             console.log('ошибка');
@@ -25,9 +25,9 @@ const TodoPage = () => {
         return (
             <div className={'p-5'}>
                 <div className={'mb-5 flex flex-col gap-0.5'}>
-                    <h3 className={'text-3xl font-black'}>Название: {data.title}</h3>
-                    <h4 className={'text-xl font-bold'}>Описание: {data.description ?
-                        data.description :
+                    <h3 className={'text-3xl font-black'}>Название: {data?.title}</h3>
+                    <h4 className={'text-xl font-bold'}>Описание: {data?.description ?
+                        data?.description :
                         <div className={'flex items-center gap-5'}>
                             <input value={description} onChange={(e) => setDescription(e.target.value)}/>
                             <button onClick={addDescription} className={'text-sm'}>добавить описание</button>
@@ -37,10 +37,6 @@ const TodoPage = () => {
                         <span className={'text-green-500'}>Выполнено</span> :
                         <span className={'text-red-500'}>Ожидает выполнения</span>}
                     </h5>
-                </div>
-                <div className={'flex items-center gap-5'}>
-                    <input type={'file'}/>
-                    <button>Добавить картинку</button>
                 </div>
             </div>
         );
